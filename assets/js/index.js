36af99e602dc5826388e6514c6437707936ad1cc
MagicalSeal1 <114394244+MagicalSeal1@users.noreply.github.com>
2025-12-18 23:32:50 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-document.querySelectorAll('.section-title').forEach(title => {
-  title.addEventListener('click', () => {
-    title.classList.toggle('open');
+document.querySelectorAll(".section-title").forEach(title => {
+  title.addEventListener("click", () => {
+    title.classList.toggle("open");
   });
 });
